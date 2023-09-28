53a58e92a57fef901c33461f54e655cadb6948f5
unknown <sanjaysingh19p@gmail.com>
2023-09-27 23:13:59 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import TodoWrapper from "./components/TodoWrapper";
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/components/TodoWrapper.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
+import EditTodoForm from "./EditTodoForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsAltH } from "@fortawesome/free-solid-svg-icons";
+
+const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const resetTodo = () => {
+    setTodos([]);
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done</h1>
+      <span style={{ color: "white" }}>Reset &nbsp;</span>
+      <FontAwesomeIcon
+        icon={faArrowsAltH}
+        onClick={resetTodo}
+        style={{ color: "white" }}
+      />
+      {/* Call resetTodo function */}
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} key={todo.id} />
+        ) : (
+          <Todo
+            task={todo}
+            key={todo.id}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default TodoWrapper;
